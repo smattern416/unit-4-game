ecb9d607b20368c026915576d6f4878e0a6fd342
Stephanie Mattern <smattern416@gmail.com>
2019-06-04 08:35:21 -0400

    added counter to javascript and alert to keep count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,10 @@
+var counter = 0;
+
 $(".crystal-image").on("click", function() {
     alert("You clicked a crystal!");
+
+    // When the user clicks the crystal, the score goes up by 1
+counter += 1;
+
+alert("You click this crystal " + counter + " times!");
 });

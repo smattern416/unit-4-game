4a31b472861ebf94ca04cc90bdca3cddda24d627
Stephanie Mattern <smattern416@gmail.com>
2019-06-04 18:14:07 -0400

    created function for a win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     var random=Math.floor(Math.random()*102+19)
     $(`#numberMatch`).text(Random);
 
-    var pink= Math.floor(Math.random()*12+1);
     var purple= Math.floor(Math.random()*12+1);
+    var pink= Math.floor(Math.random()*12+1);
     var orange= Math.floor(Math.random()*12+1);
     var yellow= Math.floor(Math.random()*12+1);
 
@@ -13,6 +13,27 @@ $(document).ready(function() {
     var userTotal= 0;
     $(`#wins`).text(wins);
     $(`#losses`).text(losses);
+
+    //  Reset the game
+    function reset() {
+        Random=Math.floor(Math.random()*102+19);
+        $(`#numberMatch`).text(Random);
+
+        purple=Math.floor(Math.random()*12+1);
+        pink=Math.floor(Math.random()*12+1);
+        orange=Math.floor(Math.random()*12+1);
+        yellow=Math.floor(Math.random()*12+1);
+        userTotal= 0;
+        $(`#totalScore`).text(userTotal);
+    }
+//   What happens when you win the game
+function win() {
+    wins++;
+    $(`#wins`).text(wins);
+    reset();
+}
+ 
+//   What happens when you lose the game
 
     //  What happens when you click on the purple crystal
  
@@ -29,5 +50,5 @@ $(document).ready(function() {
  
 //   What happens when you lose the game
 
-//  Reset the game
+
 });

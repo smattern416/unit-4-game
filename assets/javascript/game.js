1ba98ac0fb759deb80401f7343033402d7c76b91
Stephanie Mattern <smattern416@gmail.com>
2019-06-04 16:00:55 -0400

    made functions for crystals, we'll see if they work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,61 @@ $(document).ready(function() {
     var lossCount = 0; // counter for game losses
     $("#lScore").text("Losses: " + lossCount);
     var userScore = 0; // counter for user score
-    var gameScore = Math.floor(Math.random() * 102) + 19; // creates game score, a random number between 19-120
-    var crystalOne = Math.floor(Math.random() * 12) + 1; // creates crystal 1 value, a random number between 1-12
-    var crystalTwo = Math.floor(Math.random() * 12) + 1; // creates crystal 2 value, a random number between 1-12
-    var crystalThree = Math.floor(Math.random() * 12) + 1; // creates crystal 3 value, a random number between 1-12
-    var crystalFour = Math.floor(Math.random() * 12) + 1; // creates crystal 4 value, a random number between 1-12
-  
+    var gameScore = Math.floor(Math.random() * 102) + 19; 
+    var crystalOne = Math.floor(Math.random() * 12) + 1; 
+    var crystalTwo = Math.floor(Math.random() * 12) + 1; 
+    var crystalThree = Math.floor(Math.random() * 12) + 1; 
+    var crystalFour = Math.floor(Math.random() * 12) + 1;
+
+    $("#gScore").text("Game Score: " + gameScore);
+    $("#uScore").text("User Score: " + userScore);
+
+    //  What happens when you click on the purple crystal
+  $("#crystal1").click(function() {
+    userScore = userScore + crystalOne; 
+    console.log("User Score: " + userScore);
+    $("#uScore").text("User Score: " + userScore);
+      if (userScore === gameScore) {
+        winGame();
+      } else if (userScore > gameScore) {
+        loseGame();
+      }
+  });
+
+//   What happens when you click on the pink crystal
+  $("#crystal2").click(function() {
+    userScore = userScore + crystalTwo; 
+    console.log("User Score: " + userScore);
+      $("#uScore").text("User Score: " + userScore);
+      if (userScore === gameScore) {
+        winGame();
+      } else if (userScore > gameScore) {
+        loseGame();
+      }
+  });
+
+//   What happens when you click on the orange crystal
+  $("#crystal3").click(function() {
+    userScore = userScore + crystalThree; 
+    console.log("User Score: " + userScore);
+      $("#uScore").text("User Score: " + userScore);
+      if (userScore === gameScore) {
+        winGame();
+      } else if (userScore > gameScore) {
+        loseGame();
+      }
+  });
+
+//   What happens when you click on the yellow crystal
+  $("#crystal4").click(function() {
+    userScore = userScore + crystalFour; 
+    console.log("User Score: " + userScore);
+    $("#uScore").text("User Score: " + userScore);
+      if (userScore === gameScore) {
+        winGame();
+      } else if (userScore > gameScore) {
+        loseGame();
+      }
+  });
+
+});

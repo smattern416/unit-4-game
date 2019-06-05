fe8e5a5cd5bf132675a7c432e3d53c02502b5fa1
Stephanie Mattern <smattern416@gmail.com>
2019-06-04 20:38:56 -0400

    added my other if else's for other crystals... code not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     var random=Math.floor(Math.random()*102+19)
-    $(`#numberMatch`).text(Random);
+    $("#numberMatch").text(Random);
 
     var purple= Math.floor(Math.random()*12+1);
     var pink= Math.floor(Math.random()*12+1);
@@ -11,56 +11,86 @@ $(document).ready(function() {
     var wins= 0;
     var losses= 0;
     var userTotal= 0;
-    $(`#wins`).text(wins);
-    $(`#losses`).text(losses);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
 
     //  Reset the game
     function reset() {
         Random=Math.floor(Math.random()*102+19);
-        $(`#numberMatch`).text(Random);
+        $("#numberMatch").text(Random);
 
         purple=Math.floor(Math.random()*12+1);
         pink=Math.floor(Math.random()*12+1);
         orange=Math.floor(Math.random()*12+1);
         yellow=Math.floor(Math.random()*12+1);
         userTotal= 0;
-        $(`#totalScore`).text(userTotal);
+        $("#totalScore").text(userTotal);
     }
 //   What happens when you win the game
 function winning() {
     wins++;
-    $(`#wins`).text(wins);
+    $("#wins").text(wins);
     reset();
 }
  
 //   What happens when you lose the game
 function losing() {
     losses++;
-    $(`#losses`).text(losses);
+    $("#losses").text(losses);
     reset();
 }
 
     //  What happens when you click on the purple crystal
-    $(`#pink`).on(`click`, function(){
-        userTotal = userTotal + pink;
-        $(`#totalScore`).text(userTotal);
+    $(".purple").on("click", function(){
+        userTotal = userTotal + purple;
+        $("#totalScore").text(userTotal);
         
-        if(userTotal == Random){
+        if (userTotal == Random){
             winning();
         }
-        else if(userTotal > Random){
+        else if (userTotal > Random){
             losing();
         }
     })
 
 //   What happens when you click on the pink crystal
-
+$(".pink").on("click", function(){
+    userTotal = userTotal + pink;
+    $("#totalScore").text(userTotal);
+    
+    if (userTotal == Random){
+        winning();
+    }
+    else if (userTotal > Random){
+        losing();
+    }
+})
 
 //   What happens when you click on the orange crystal
-
+$(".orange").on("click", function(){
+    userTotal = userTotal + orange;
+    $("#totalScore").text(userTotal);
+    
+    if (userTotal == Random){
+        winning();
+    }
+    else if (userTotal > Random){
+        losing();
+    }
+})
 
 //   What happens when you click on the yellow crystal
 
-
+$(".yellow").on("click", function(){
+    userTotal = userTotal + yellow;
+    $("#totalScore").text(userTotal);
+    
+    if (userTotal == Random){
+        winning();
+    }
+    else if (userTotal > Random){
+        losing();
+    }
+});
 
 });
